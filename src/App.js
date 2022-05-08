282ffdb06cc4ec6d1574573cb0a9badb98e0b85a
Michael Kozubsi <mkozubski@academic.rrc.ca>
2022-05-07 21:51:53 -0500

    Installed React-Animate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <div className="App-banner">
+        <img src="images/MLogo.png" className="App-logo" alt="logo" />
+        <p className='textLogo'>This is a test</p>
     </div>
+    <div className='App-body'>
+
+    </div>
+    </div>
+    
   );
 }
 
